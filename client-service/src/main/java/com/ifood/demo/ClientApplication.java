8e8e26ab78fa25ba01de95af57f818eab71446ea
Your name <email@example.com>
2018-02-09 17:36:27 -0200

    implemented rabbitmq

--- client-service/src/main/java/com/ifood/demo/ClientApplication.java
+++ client-service/src/main/java/com/ifood/demo/ClientApplication.java
@@ -1,5 +1,10 @@
 package com.ifood.demo;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,15 +13,36 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import com.ifood.demo.client.ClientEventHandler;
 
 @SpringBootApplication
+@EnableRabbit
 @EnableJpaRepositories("com.ifood.demo")
 public class ClientApplication {
-	
-	public static void main(String[] args) {
+
+    final static String queueName = "client-queue";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    public static TopicExchange exchange() {
+        return new TopicExchange("event-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+
+
+    public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
-	
+
+
     @Bean
     ClientEventHandler clientEventHandler() {
         return new ClientEventHandler();
     }
+
 }
--- client-service/src/main/java/com/ifood/demo/client/ClientEventHandler.java
+++ client-service/src/main/java/com/ifood/demo/client/ClientEventHandler.java
@@ -1,5 +1,11 @@
 package com.ifood.demo.client;
 
+import com.ifood.demo.ClientApplication;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.HandleAfterDelete;
 import org.springframework.data.rest.core.annotation.HandleAfterSave;
@@ -11,18 +17,29 @@ import lombok.extern.slf4j.Slf4j;
 @RepositoryEventHandler(Client.class)
 public class ClientEventHandler {
 
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
 	@HandleAfterCreate
 	public void handleClientCreate(Client c) {
+
+		String routingKey = "customer.created";
+		String message = "customer created";
+
+		rabbitTemplate.convertAndSend(ClientApplication.exchange().getName(), routingKey, c.toString());
+
 		log.info("handleClientCreate: {}", c.getId());
 	}
 	
 	@HandleAfterSave
 	public void handleClientSave(Client c) {
+
 		log.info("handleClientSave: {}", c.getId());
 	}
 	
 	@HandleAfterDelete
 	public void handleClientDelete(Client c) {
+
 		log.info("handleClientDelete: {}", c.getId());
 	}
 }
