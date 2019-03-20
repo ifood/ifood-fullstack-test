2bed73d092c1f7780408a1fc390bc30769abf255
Luis Mesquita <luislcbarroso@gmail.com>
2019-03-20 01:21:56 -0300

    Added a proxy-service for swagger documentation

--- proxy-service/src/test/java/com/ifood/proxyservice/ProxyServiceApplicationTests.java
+++ proxy-service/src/test/java/com/ifood/proxyservice/ProxyServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ifood.configservice;
+package com.ifood.proxyservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigServiceApplicationTests {
+public class ProxyServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
