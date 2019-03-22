7dc07dfa69d5486af53289caa1d25b1ee7b187ef
Luis Barroso <luis@mobiclub.com.br>
2019-03-22 14:21:31 -0300

    Filter form added

--- ifood-fullstack-test-frontend/src/App.js
+++ ifood-fullstack-test-frontend/src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import store from './store';
+import Paper from '@material-ui/core/Paper';
+
+import HeaderContainer from './containers/HeaderContainer';
+import TableContainer from './containers/TableContainer';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+      	<Paper>
+          <h1>
+        		Order List
+        	</h1>
+          <HeaderContainer/>
+          <TableContainer/>
+        </Paper>
+      </Provider>
     );
   }
 }
